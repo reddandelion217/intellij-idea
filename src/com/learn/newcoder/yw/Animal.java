422be51b3f6ceb4eb877611cb52697bbae11e06b
reddandelion217 <1274529373@qq.com>
2016-06-28 11:45:41 +0800

    第二次提交

---
+++ src/com/learn/newcoder/yw/Animal.java
@@ -0,0 +1,10 @@
+package com.learn.newcoder.yw;
+
+/**
+ * Created by Administrator on 2016/6/28.
+ */
+public class Animal {
+    public static void main(String[] args){
+    }
+
+}
---
+++ src/com/learn/newcoder/yw/Main.java
@@ -0,0 +1,10 @@
+package com.learn.newcoder.yw;
+
+/**
+ * Created by Administrator on 2016/6/27.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello main");
+    }
+}
